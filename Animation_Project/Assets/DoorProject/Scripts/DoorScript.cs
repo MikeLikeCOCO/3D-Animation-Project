ae54fb6223010bb90d0b81390275e0ac4fb92b3b
Hussein EL-Husseini <husseine2985@gmail.com>
2023-04-10 17:26:40 +1000

    simple building with doors
    new door anims

---
+++ Animation_Project/Assets/DoorProject/Scripts/DoorScript.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+    private Animator _animator;
+
+    public GameObject OpenPanel = null;
+
+    private bool _isInsideTrigger = false;
+    private int qTrigger = 0; // count OnTriggerEnter
+   
+	// Use this for initialization
+	void Start () {
+        _animator = transform.Find("Door").GetComponent<Animator>();
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+          //  qTrigger++;
+            _isInsideTrigger = true;
+            if(_animator.GetBool("open") == false) {
+                OpenPanel.SetActive(true);
+            }
+            
+        }
+    }
+
+    // void OnTriggerExit(Collider other)
+    // {
+        
+    //     if (other.tag == "Player")
+    //     {
+            
+    //         _isInsideTrigger = false;
+            
+    //         _animator.SetBool("open", false);
+    //         OpenPanel.SetActive(false);
+    //     }
+    // }
+
+
+     void OnTriggerExit(Collider other)
+    {
+       // Debug.Log(qTrigger);
+       _isInsideTrigger = false;
+       OpenPanel.SetActive(false);
+        StartCoroutine(closeDoor(other));
+    }
+    IEnumerator closeDoor(Collider other)
+    {
+        float timeToWait = 4f;
+        yield return new WaitForSeconds(timeToWait);
+        if (other.tag == "Player")
+        {
+          //  qTrigger--;
+           // if(qTrigger == 0) {
+            
+            
+            _animator.SetBool("open", false);
+            
+          //  }
+
+        }
+    }
+
+    private bool IsOpenPanelActive
+    {
+        get
+        {
+            return OpenPanel.activeInHierarchy;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if(IsOpenPanelActive && _isInsideTrigger)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                OpenPanel.SetActive(false);
+                _animator.SetBool("open", true);  
+            }
+        }
+	}
+}
